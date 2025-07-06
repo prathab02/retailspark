14b9b383b69f8052a80c74328aaedef91345aacf
Prathab <cloudprathab@gmail.com>
2025-07-06 19:35:35 -0400

    Update gcp_pyspark_schedule.sh
    changing the code location to my cloud cloudstorage location

--- gcp_pyspark_schedule.sh
+++ gcp_pyspark_schedule.sh
@@ -1,4 +1,4 @@
-gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/mohamedirfan_mohideen/project/Usecase4_GcpGcsReadWritehive_cloud.py
+gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/cloudprathab/project/Usecase4_GcpGcsReadWritehive_cloud.py
 #Alternative for spark-submit inside the dataproc cluster
 #spark-submit --master yarn --mode client --num-executors 4….
 if [ $? -ne 0 ]
