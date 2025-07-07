df061e45b4f8402cba00860630bfcda71dfefd16
Prathab <cloudprathab@gmail.com>
2025-07-06 20:17:58 -0400

    Update Usecase4_GcpGcsReadWritehive_cloud.py

--- Usecase4_GcpGcsReadWritehive_cloud.py
+++ Usecase4_GcpGcsReadWritehive_cloud.py
@@ -1,19 +1,11 @@
-#prerequisites
-# Download the jar from https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-latest-hadoop2.jar
-#spark-submit --jars /home/hduser/install/gcp/gcs-connector-latest-hadoop2.jar GcpGcsReadWritehivewe39_cloud.py
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 def main():
    from pyspark.sql import SparkSession
    # define spark configuration object
-   spark = SparkSession.builder\
-      .appName("GCP GCS Hive Read/Write") \
-      .enableHiveSupport()\
-      .getOrCreate()
-
-   print("Use Spark Application to Read csv data from cloud GCS+Hive and get a DF created with the GCS data in the GCP, "
-         "convert csv to json in the DF and store the json into new cloud GCS+Hive location")
-   print("Hive to GCS to hive starts here")
+   spark = SparkSession.builder.appName("GCP GCS Hive Read/Write").enableHiveSupport().getOrCreate()
+   print("Use Spark Application to Read csv data from cloud GCS+Hive and get a DF created with the GCS data in the GCP, convert csv to json in the DF and store the json into new cloud GCS+Hive location")
+   #print("Hive to GCS to hive starts here")
    custstructtype1 = StructType([StructField("id", IntegerType(), False),
                               StructField("custfname", StringType(), False),
                               StructField("custlname", StringType(), True),
@@ -37,5 +29,4 @@ def main():
    print(curts)
    gcs_df.repartition(2).write.mode("overwrite").csv("gs://prathab-wd34/output/dataset/cust_csv")
    print("gcs Write Completed Successfully")
-
 main()
